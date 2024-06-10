925d3b7d3288d1c1b361d7606da45acf43a0a164
jeewanlamsal <106655205+jeewanlamsal@users.noreply.github.com>
2024-06-10 01:49:06 +0000

    container config done

---
+++ .devcontainer/02.js
@@ -0,0 +1,14 @@
+"use strict";   //treat all js code as newer version
+// alert (3+3) //we are using nodejs,not browser
+console.log(3
+    +3) //  code readability should be high
+console.log("Jeewan")
+
+
+
+let name="Jeewan"
+let age=20
+let isLoggedIn=false
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //object
